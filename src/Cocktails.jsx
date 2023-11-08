73dc3b50771c289c34826fcfa91d0a4df4e2fbf1
jemma <jemma_clark1@hotmail.com>
2023-11-08 11:55:32 +1100

    cocktail fetch working

--- src/Cocktails.jsx
+++ src/Cocktails.jsx
@@ -1,44 +1,102 @@
 import React, { useState } from "react";
 
+const availableCocktails = [
+  "Vodka",
+  "Gin",
+  "Dark Rum",
+  "Bourbon",
+  "Cointreau",
+  "Amaretto",
+  "Light Rum",
+  "Beer",
+  "Wine",
+  "Brandy",
+  "Baileys",
+  "Absinthe",
+  "Champagne",
+  "Tequila",
+  "Dry Vermouth",
+  "Scotch",
+  "Cognac",
+  "Ginger Beer",
+  "Whiskey",
+  "Aperol",
+  // Add any other liquors here
+];
+
 function CocktailApp() {
-  const [cocktailData, setCocktailData] = useState([]);
   const [selectedAlcohol, setSelectedAlcohol] = useState("");
   const [randomCocktail, setRandomCocktail] = useState(null);
 
-  function handleFetchCocktails() {
+  function handleFindCocktail() {
     if (selectedAlcohol) {
       const apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${selectedAlcohol}`;
       fetch(apiUrl)
         .then((response) => response.json())
         .then((data) => {
-          console.log(data); // Debug: Log the data to the console
-          setCocktailData(data.drinks || []);
+          console.log(data);
+
+          const drinks = data.drinks || [];
+          if (drinks.length > 0) {
+            const randomIndex = Math.floor(Math.random() * drinks.length);
+            const selectedCocktail = drinks[randomIndex];
+
+            fetchCocktailDetailsById(selectedCocktail.idDrink);
+          } else {
+            setRandomCocktail(null);
+          }
         })
         .catch((error) => console.error(error));
     }
   }
 
-  function handleFindCocktail() {
-    if (cocktailData.length > 0) {
-      const randomIndex = Math.floor(Math.random() * cocktailData.length);
-      const selectedCocktail = cocktailData[randomIndex];
-      setRandomCocktail(selectedCocktail);
+  // Function to fetch cocktail details by ID
+  function fetchCocktailDetailsById(cocktailId) {
+    const apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`;
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const cocktailDetails = data.drinks ? data.drinks[0] : null;
+        setRandomCocktail(cocktailDetails);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  function getIngredientsList(cocktail) {
+    const ingredients = [];
+
+    for (let i = 1; i <= 15; i++) {
+      const ingredient = cocktail[`strIngredient${i}`];
+      const measure = cocktail[`strMeasure${i}`];
+
+      if (ingredient) {
+        ingredients.push(`${measure} ${ingredient}`);
+      } else {
+        break;
+      }
     }
+
+    return ingredients;
   }
 
   return (
     <div>
       <h1>Cocktail App</h1>
       <div>
-        <label>
-          Select Alcohol:
-          <input
-            type="text"
-            value={selectedAlcohol}
-            onChange={(e) => setSelectedAlcohol(e.target.value)}
-          />
-        </label>
-        <button onClick={handleFetchCocktails}>Fetch Cocktails</button>
+        <label>Select a Cocktail or Liquor:</label>
+        {availableCocktails.map((cocktail) => (
+          <label key={cocktail}>
+            <input
+              type="radio"
+              name="selectedAlcohol"
+              value={cocktail}
+              checked={selectedAlcohol === cocktail}
+              onChange={() => setSelectedAlcohol(cocktail)}
+            />
+            {cocktail}
+          </label>
+        ))}
       </div>
 
       <button onClick={handleFindCocktail}>Find Cocktail</button>
@@ -47,11 +105,17 @@ function CocktailApp() {
         <div>
           <h2>Random Cocktail:</h2>
           <h3>Name: {randomCocktail.strDrink}</h3>
+          <h4>Ingredients:</h4>
+          <ul>
+            {getIngredientsList(randomCocktail).map((ingredient, i) => (
+              <li key={i}>{ingredient}</li>
+            ))}
+          </ul>
+          <p>Instructions: {randomCocktail.strInstructions}</p>
           <img
             src={randomCocktail.strDrinkThumb}
             alt={randomCocktail.strDrink}
           />
-          {/* Add more cocktail information here */}
         </div>
       )}
     </div>
